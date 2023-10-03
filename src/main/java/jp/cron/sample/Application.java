40cbe49f6df625707fdffc922abe90671803185e
cron <64413934+cronree-91@users.noreply.github.com>
2023-10-03 21:04:55 +0900

    settings-server property

--- src/main/java/jp/cron/sample/Application.java
+++ src/main/java/jp/cron/sample/Application.java
@@ -5,13 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class)
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        environment.getPropertySources().addLast(new SettingsServerPropertySource());
+        new SpringApplicationBuilder(Application.class)
+                .environment(environment)
                 .web(WebApplicationType.NONE).run(args);
     }
 

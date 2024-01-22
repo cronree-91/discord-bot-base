b10077c98cd30a6377603f50b7a99d9d032ea946
cronree-91 <cron@cron.jp>
2024-01-22 10:36:32 +0900

    fix: thread locked

--- src/main/java/jp/cron/sample/CommandLineRunner.java
+++ src/main/java/jp/cron/sample/CommandLineRunner.java
@@ -23,7 +23,7 @@ public class CommandLineRunner implements org.springframework.boot.CommandLineRu
                 .build();
 
         while (jda.getStatus() != JDA.Status.SHUTDOWN) {
-            Thread.onSpinWait();
+            Thread.sleep(100L);
         }
 
         getLogger().warn("SHUTTING DOWN...");
